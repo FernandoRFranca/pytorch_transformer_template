b96fb6ddfc395be042a3d501369960c039ea72b0
zfern <zfern@FERNANDO-DELL>
2024-03-21 15:47:18 -0300

    Implements ResidualConnection class.

--- model.py
+++ model.py
@@ -113,3 +113,13 @@ class MultiHeadAttentionBlock(nn.Module):
 
         # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
         return self.w_o(x)
+
+class ResidualConnection(nn.Module):
+
+    def __init__(self, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer)
